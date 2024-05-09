d42736ae19f6c5cbfb642590acf72251b358796f
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2024-05-08 23:49:44 -0300

    fix: get more detailed error toast

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,41 +1,40 @@
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
 });
 
-
 const Api = {
   getCollectPoints: async (params) => {
     try {
-      const response = await api.get('/collect-points', { params });
+      const response = await api.get("/collect-points", { params });
       return response.data;
     } catch (error) {
-      throw new Error('Erro ao buscar pontos de coleta: ' + error.message);
+      throw new Error("Erro ao buscar pontos de coleta: " + error.message);
     }
   },
 
   createCollectPoint: async (collectPointData) => {
     try {
-      const response = await api.post('/collect-points', collectPointData);
+      const response = await api.post("/collect-points", collectPointData);
       return response.data;
     } catch (error) {
-      toast.error('Erro ao cadastrar ponto de coleta')
-      throw new Error('Erro ao cadastrar ponto de coleta: ' + error);
+      toast.error(
+        "Erro ao cadastrar ponto de coleta: " + error?.response?.data?.message
+      );
+      throw new Error("Erro ao cadastrar ponto de coleta: " + error);
     }
   },
   getStates: async () => {
     try {
-      const response = await api.get('/location/states');
+      const response = await api.get("/location/states");
       return response.data;
     } catch (error) {
-      throw new Error('Erro ao consultar estados: ' + error.message);
+      throw new Error("Erro ao consultar estados: " + error.message);
     }
   },
   getCitiesByState: async (state) => {
@@ -43,9 +42,9 @@ const Api = {
       const response = await api.get(`/location/cities/${state}`);
       return response.data;
     } catch (error) {
-      throw new Error('Erro ao consultar cidades: ' + error.message);
+      throw new Error("Erro ao consultar cidades: " + error.message);
     }
-  }
+  },
 };
 
 export default Api;
